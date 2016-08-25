089f42f975c64e023f334de5bfd1a098a20a2ef9
GalloElief <elief.gallo.fad@gmail.com>
2016-08-25 16:49:51 -0500

    agregue imagenes y estilos

---
+++ js/estilos.js
@@ -0,0 +1,18 @@
+
+<script src="js/tagcanvas.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+window.onload = function() {
+try {
+TagCanvas.Start('myCanvas','tags',{
+textColour: 'black',
+outlineColour: '#ff00ff',
+reverse: true,
+depth: 0.8,
+maxSpeed: 0.05
+});
+} catch(e) {
+// something went wrong, hide the canvas container
+document.getElementById('myCanvasContainer').style.display = 'none';
+}
+};
+</script>
